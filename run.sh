d6180e5fc8bd9ba4cd661af5a8cc0affa1f32895
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2018-09-10 18:18:43 +0800

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 
-/usr/local/MATLAB/R2014a/bin/matlab
+
+
+if [ $1 = build ]
+then
+    /usr/local/MATLAB/R2014a/bin/matlab -nodesktop -r "run('initPmtk3.m'), run('testPmtk3.m'), exit()"
+
+elif [ "$1" = matlab ]
+then
+    /usr/local/MATLAB/R2014a/bin/matlab
+
+elif [ "$1" = "demos/isingImageDenoiseDemo.m" ]
+then
+#    ./run.sh "demos/isingImageDenoiseDemo.m"
+    cd demos
+    /usr/local/MATLAB/R2014a/bin/matlab -nodesktop -nosplash -r "run('isingImageDenoiseDemo.m')"
+
+else
+    echo Pass
+fi
